5f10d34d68c4c64aa11662dea5820c11b0bf0feb
Christof <bauer.christof@gmail.com>
2021-12-09 20:10:08 -0500

    Update testimonials.js

--- pages/testimonials.js
+++ pages/testimonials.js
@@ -18,24 +18,24 @@ const Testimonials = () => {
 
       <div className="flex flex-col items-center justify-content px-4 md:px-8 py-16 md:py-32">
         <h1 className="text-4xl mb-2">Testimonials</h1>
-        <p className="mb-12 max-w-sm text-center">Let's see how students described their experience with shift_happens_yoga:</p>
-        {/* <section className="flex items-center justify-center"> */}
-        <div className="relative overflow-hidden w-full pt-[56.25%]">
-          <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fweb.facebook.com%2Fshahar.a.yadid%2Fposts%2F10215294421014106&show_text=true" className="absolute top-0 left-0 right-0 bottom-0 h-full w-full border-none overflow-hidden shadow" scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-        </div>
-        <div className="relative overflow-hidden w-full pt-[56.25%]">
-          <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fweb.facebook.com%2Fmarc.radtke%2Fposts%2F2826244507399318&show_text=true" className="absolute top-0 left-0 right-0 bottom-0 h-full w-full border-none overflow-hidden shadow" scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-        </div>
-        <div className="relative overflow-hidden w-full pt-[56.25%]">
-          <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fweb.facebook.com%2Fty.maryna%2Fposts%2F10156362442802034&show_text=true" className="absolute top-0 left-0 right-0 bottom-0 h-full w-full border-none overflow-hidden shadow" scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-        </div>
-        <div className="relative overflow-hidden w-full pt-[56.25%]">
-          <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fweb.facebook.com%2Ftatayikwantse%2Fposts%2F2673583402869532&show_text=true" className="absolute top-0 left-0 right-0 bottom-0 h-full w-full border-none overflow-hidden shadow" scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-        </div>
-        <div className="relative overflow-hidden w-full pt-[56.25%]">
-          <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fweb.facebook.com%2Fchristiane.eitle%2Fposts%2F1971860406185436&show_text=true" className="absolute top-0 left-0 right-0 bottom-0 h-full w-full border-none overflow-hidden shadow" scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-        </div>
-        {/* </section> */}
+        <p className="mb-12 max-w-sm text-center">Let&apos;s see how students described their experience with shift_happens_yoga:</p>
+        <section className="flex items-center justify-center">
+          <div className="relative overflow-hidden w-full pt-[56.25%]">
+            <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fweb.facebook.com%2Fshahar.a.yadid%2Fposts%2F10215294421014106&show_text=true&appId=239066989437628" className="absolute top-0 left-0 right-0 bottom-0 h-full w-full border-none overflow-hidden shadow" scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture;"></iframe>
+          </div>
+          <div className="relative overflow-hidden w-full pt-[56.25%]">
+            <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fweb.facebook.com%2Fmarc.radtke%2Fposts%2F2826244507399318&show_text=true&appId=239066989437628" className="absolute top-0 left-0 right-0 bottom-0 h-full w-full border-none overflow-hidden shadow" scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture;"></iframe>
+          </div>
+          <div className="relative overflow-hidden w-full pt-[56.25%]">
+            <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fweb.facebook.com%2Fty.maryna%2Fposts%2F10156362442802034&show_text=true&appId=239066989437628" className="absolute top-0 left-0 right-0 bottom-0 h-full w-full border-none overflow-hidden shadow" scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture;"></iframe>
+          </div>
+          <div className="relative overflow-hidden w-full pt-[56.25%]">
+            <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fweb.facebook.com%2Ftatayikwantse%2Fposts%2F2673583402869532&show_text=true&appId=239066989437628" className="absolute top-0 left-0 right-0 bottom-0 h-full w-full border-none overflow-hidden shadow" scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture;"></iframe>
+          </div>
+          <div className="relative overflow-hidden w-full pt-[56.25%]">
+            <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fweb.facebook.com%2Fchristiane.eitle%2Fposts%2F1971860406185436&show_text=true&appId=239066989437628" className="absolute top-0 left-0 right-0 bottom-0 h-full w-full border-none overflow-hidden shadow" scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture;"></iframe>
+          </div>
+        </section>
         {/* <div id="fb-root" /> */}
       </div>
     </div>
