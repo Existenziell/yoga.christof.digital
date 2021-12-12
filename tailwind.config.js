74ab51a56ae64b5827b4a594cd2c63d67abaa0f5
Christof <bauer.christof@gmail.com>
2021-12-12 00:05:44 -0500

    [Debug] Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // false or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
         // https://tailwindcss.com/docs/font-family#customizing
-        body: [
+        sans: [
           'Gotu',
         ],
       },
