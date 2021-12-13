0d39b5797e15d1b3f43fa2f3695b5c9397525def
Christof <bauer.christof@gmail.com>
2021-12-12 20:31:10 -0500

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,30 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
 const Footer = () => {
+  const router = useRouter()
+
   return (
-    <footer
-      className='flex items-center justify-center w-full py-1 static bottom-0 text-xs
-      bg-cloth-pattern dark:bg-none dark:bg-brand-dark border-t dark:border-none'>
+    <footer className='static bottom-0 flex items-center justify-center w-full py-1 text-xs
+    text-gray-500 transition-all bg-cloth-pattern border-t border-black border-opacity-10
+      dark:bg-none dark:bg-brand-dark dark:border-brand dark:border-opacity-10'>
+
       <a href='#top' aria-label='Scroll back up'>
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-16 text-gray-700 hover:text-brand transition-all" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 hover:text-brand" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
         </svg>
       </a>
+
+      {router.pathname !== '/' &&
+        <Link href='/'>
+          <a className='absolute left-0'>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 hover:text-brand" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+            </svg>
+          </a>
+        </Link>
+      }
+
     </footer>
   )
 }
