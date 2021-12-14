1a060884439c80d2bf730ab4b2ffa04e533754e7
Christof <bauer.christof@gmail.com>
2021-12-13 21:10:28 -0500

    Update ScrollIndicator.js

--- components/ScrollIndicator.js
+++ components/ScrollIndicator.js
@@ -16,7 +16,7 @@ export const ScrollIndicator = () => {
         strokeWidth='5'
         stroke='currentColor'
         strokeDasharray='0 1'
-        strokeLinecap={'round'}
+        strokeLinecap='round'
         d='M 0, 20 a 20, 20 0 1,0 40,0 a 20, 20 0 1,0 -40,0'
         style={{
           pathLength,
